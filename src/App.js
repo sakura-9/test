a765d62fd478277bdacecafcc48bac6bbb9a62be
sakura <59141860+sakura-9@users.noreply.github.com>
2022-01-16 09:37:56 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const App = () => {
+  const click = () => alert("button click");
   return (
     <>
       <h1> hallo world </h1>
+      <button onClick={click}>ボタン</button>
     </>
   );
 };
